7b0069417ec8063eb035b864dea34db27ef46400
mmvanheusden <50550545+mmvanheusden@users.noreply.github.com>
2022-01-08 11:30:17 +0100

    Fix others and clean up
    omggg my code is soo bad jesus

--- src/main/kotlin/BowBombModule.kt
+++ src/main/kotlin/BowBombModule.kt
@@ -6,8 +6,8 @@ import net.minecraft.init.Items
 import net.minecraft.network.play.client.CPacketEntityAction
 import net.minecraft.network.play.client.CPacketPlayer
 import net.minecraft.network.play.client.CPacketPlayerDigging
+import net.minecraft.network.play.client.CPacketPlayerTryUseItem
 import net.minecraft.util.EnumHand
-import org.lwjgl.input.Keyboard
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -27,29 +27,41 @@ internal object BowBombModule : PluginModule(
     description = "Increases projectile velocity on some servers",
     pluginMain = BowBombPluginMain
 ) {
-    private val spoofs by setting("Spoofs", 1, 1..100, 10)
+    private val spoofs by setting("Spoofs", 10, 1..100, 10)
     private val others by setting("Others",true, description = "Enables BowBomb for eggs, snowballs and pearls.")
     private val bypass by setting("Bypass",false)
     init {
         safeListener<PacketEvent.Send> {
-            if (it.packet !is CPacketPlayerDigging) return@safeListener
-            // TODO: make this line more readable
-            if ((it.packet as CPacketPlayerDigging).action == CPacketPlayerDigging.Action.RELEASE_USE_ITEM  && (mc.player.getHeldItem(EnumHand.MAIN_HAND).item == Items.BOW || mc.player.getHeldItem(EnumHand.OFF_HAND).item == Items.BOW) || mc.player.getHeldItem(EnumHand.MAIN_HAND).item == Items.SNOWBALL && others || mc.player.getHeldItem(EnumHand.OFF_HAND).item == Items.SNOWBALL && others || mc.player.getHeldItem(EnumHand.MAIN_HAND).item == Items.ENDER_PEARL && others || mc.player.getHeldItem(EnumHand.OFF_HAND).item == Items.ENDER_PEARL && others || mc.player.getHeldItem(EnumHand.MAIN_HAND).item == Items.EGG && others || mc.player.getHeldItem(EnumHand.OFF_HAND).item == Items.EGG && others) {
-
-                connection.sendPacket(CPacketEntityAction(mc.player, CPacketEntityAction.Action.START_SPRINTING))
-                repeat (spoofs) {
-                    val sin = -sin(Math.toRadians(mc.player.rotationYaw.toDouble()))
-                    val cos = cos(Math.toRadians(mc.player.rotationYaw.toDouble()))
-                    if (bypass) { //idk if this does anything special, i saw this being done in the first link
-                        connection.sendPacket(CPacketPlayer.Position(mc.player.posX + (sin * 100), mc.player.posY + 5, mc.player.posZ + (cos * 100), false))
-                        connection.sendPacket(CPacketPlayer.Position(mc.player.posX - (sin * 100), mc.player.posY, mc.player.posZ - (cos * 100), true))
-                    } else {
-                        connection.sendPacket(CPacketPlayer.Position(mc.player.posX - (sin * 100), mc.player.posY, mc.player.posZ - (cos * 100), true))
-                        connection.sendPacket(CPacketPlayer.Position(mc.player.posX + (sin * 100), mc.player.posY + 5, mc.player.posZ + (cos * 100), false))
+            if (it.packet is CPacketPlayerDigging) {
+                if ((it.packet as CPacketPlayerDigging).action == CPacketPlayerDigging.Action.RELEASE_USE_ITEM && (mc.player.getHeldItem(EnumHand.MAIN_HAND).item == Items.BOW)) {
+                    connection.sendPacket(CPacketEntityAction(mc.player, CPacketEntityAction.Action.START_SPRINTING))
+                    repeat(spoofs) {
+                        val sin = -sin(Math.toRadians(mc.player.rotationYaw.toDouble()))
+                        val cos = cos(Math.toRadians(mc.player.rotationYaw.toDouble()))
+                        if (bypass) { //idk if this does anything special, i saw this being done in the first link
+                            connection.sendPacket(CPacketPlayer.Position(mc.player.posX + (sin * 100), mc.player.posY + 5, mc.player.posZ + (cos * 100), false))
+                            connection.sendPacket(CPacketPlayer.Position(mc.player.posX - (sin * 100), mc.player.posY, mc.player.posZ - (cos * 100), true))
+                        } else {
+                            connection.sendPacket(CPacketPlayer.Position(mc.player.posX - (sin * 100), mc.player.posY, mc.player.posZ - (cos * 100), true))
+                            connection.sendPacket(CPacketPlayer.Position(mc.player.posX + (sin * 100), mc.player.posY + 5, mc.player.posZ + (cos * 100), false))
+                        }
                     }
                 }
-            }
+            } else if (it.packet is CPacketPlayerTryUseItem && others) {
+                if ((mc.player.getHeldItem(EnumHand.MAIN_HAND).item == Items.EGG) || (mc.player.getHeldItem(EnumHand.MAIN_HAND).item == Items.ENDER_PEARL) || (mc.player.getHeldItem(EnumHand.MAIN_HAND).item == Items.SNOWBALL)) {
+                    repeat(spoofs) {
+                        val sin = -sin(Math.toRadians(mc.player.rotationYaw.toDouble()))
+                        val cos = cos(Math.toRadians(mc.player.rotationYaw.toDouble()))
+                        if (bypass) { //idk if this does anything special, i saw this being done in the first link
+                            connection.sendPacket(CPacketPlayer.Position(mc.player.posX + (sin * 100), mc.player.posY + 5, mc.player.posZ + (cos * 100), false))
+                            connection.sendPacket(CPacketPlayer.Position(mc.player.posX - (sin * 100), mc.player.posY, mc.player.posZ - (cos * 100), true))
+                        } else {
+                            connection.sendPacket(CPacketPlayer.Position(mc.player.posX - (sin * 100), mc.player.posY, mc.player.posZ - (cos * 100), true))
+                            connection.sendPacket(CPacketPlayer.Position(mc.player.posX + (sin * 100), mc.player.posY + 5, mc.player.posZ + (cos * 100), false))
+                        }
+                    }
+                }
+            } else return@safeListener
         }
-        bind.value.setBind(Keyboard.KEY_G)
     }
 }
